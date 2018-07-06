17bef6a9d3f935104f37505074e5fcaa283f8b68
Mateo Garcia <mateog@stanford.edu>
2018-07-06 13:27:41 -0700

    Implements socket-io and logs reply messages in the console. Next up: Translate local commands to socket-io commands

--- app.js
+++ app.js
@@ -1,18 +1,36 @@
-var app = {
-  name: 'app.js',
-  startDate: new Date(),
-};
+var socket = io.connect('http://localhost');
+socket.on('reply', function (data) {
+  console.log('socket-io: reply -' + data);
+});
 
-const ip = 'localhost';
-const port = 32002;
 
-var initialize = function(callback) {
-  var net = require('net');
-  var client = new net.Socket();
-  client.connect(port, ip, function() {
-    callback(null, client);
-  });
-  client.on('error', function(err) {
-    callback(err, client);
-  });
-};
+// Draw blue points top to bottom, left to right across entire canvas
+// var r = 0;
+// var c = 0;
+// var runInt = setInterval(function() {
+//   drawPoint(projector.ofApp, c, r++, [0, 0, 255, 1]);
+//   if (r >= canvasHeight) {
+//     r = 0;
+//     c++;
+//   }
+// }, 500);
+
+// Draw two sides of an obtuse blue triangle from top left to center to middle right
+// var runInt = setInterval(function() {
+//   let vertices = [[0, 0], [canvasWidth / 2, canvasHeight / 2], [canvasWidth, canvasHeight / 2]];
+//   drawLine(projector.ofApp, vertices, [0, 0, 255, 1]);
+// }, 5000);
+
+// Draw a filled blue triangle from top left to center to middle left
+// var runInt = setInterval(function() {
+//   drawShape(projector.ofApp, [[0, 0], [canvasWidth / 2, canvasHeight / 2], [0, canvasHeight / 2]], [0, 0, 255, 1], true);
+// }, 5000);
+
+// Draw a filled blue ellipse in the center of the screen.
+// var runInt = setInterval(function() {
+//   let x = canvasWidth / 2;
+//   let y = canvasHeight / 2;
+//   let w = canvasWidth / 2;
+//   let h = canvasHeight / 2;
+//   drawEllipse(projector.ofApp, x, y, w, h, [0, 0, 255, 1], true);
+// }, 5000);
