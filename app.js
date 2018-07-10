2c2b0110df2b3f784ee1551c4e08144ba892301f
Mateo Garcia <mateog@stanford.edu>
2018-07-10 13:36:06 -0700

    Executes run loop.

--- app.js
+++ app.js
@@ -9,33 +9,45 @@ socket.on('reply', function (data) {
 });
 
 
-// Draw blue points top to bottom, left to right across entire canvas
-// var r = 0;
-// var c = 0;
-// var runInt = setInterval(function() {
-//   drawPoint(projector.ofApp, c, r++, [0, 0, 255, 1]);
-//   if (r >= canvasHeight) {
-//     r = 0;
-//     c++;
-//   }
-// }, 500);
+var runLoop = function() {
+  // Draw blue points top to bottom, left to right across entire canvas
+  // var r = 0;
+  // var c = 0;
+  // var runInt = setInterval(function() {
+  //   drawPoint(projector.ofApp, c, r++, [0, 0, 255, 1]);
+  //   if (r >= canvasHeight) {
+  //     r = 0;
+  //     c++;
+  //   }
+  // }, 500);
 
-// Draw two sides of an obtuse blue triangle from top left to center to middle right
-// var runInt = setInterval(function() {
-//   let vertices = [[0, 0], [canvasWidth / 2, canvasHeight / 2], [canvasWidth, canvasHeight / 2]];
-//   drawLine(projector.ofApp, vertices, [0, 0, 255, 1]);
-// }, 5000);
+  // Draw two sides of an obtuse blue triangle from top left to center to middle right
+  // var runInt = setInterval(function() {
+  //   let vertices = [[0, 0], [canvasWidth / 2, canvasHeight / 2], [canvasWidth, canvasHeight / 2]];
+  //   drawLine(projector.ofApp, vertices, [0, 0, 255, 1]);
+  // }, 5000);
+  
+  // Draw a filled blue triangle from top left to center to middle left
+  // var runInt = setInterval(function() {
+  //   drawShape(projector.ofApp, [[0, 0], [canvasWidth / 2, canvasHeight / 2], [0, canvasHeight / 2]], [0, 0, 255, 1], true);
+  // }, 5000);
 
-// Draw a filled blue triangle from top left to center to middle left
-// var runInt = setInterval(function() {
-//   drawShape(projector.ofApp, [[0, 0], [canvasWidth / 2, canvasHeight / 2], [0, canvasHeight / 2]], [0, 0, 255, 1], true);
-// }, 5000);
+  // Draw a filled blue ellipse in the center of the screen.
+  // var runInt = setInterval(function() {
+  //   let x = canvasWidth / 2;
+  //   let y = canvasHeight / 2;
+  //   let w = canvasWidth / 2;
+  //   let h = canvasHeight / 2;
+  //   drawEllipse(projector.ofApp, x, y, w, h, [0, 0, 255, 1], true);
+  // }, 5000);
 
-// Draw a filled blue ellipse in the center of the screen.
-// var runInt = setInterval(function() {
-//   let x = canvasWidth / 2;
-//   let y = canvasHeight / 2;
-//   let w = canvasWidth / 2;
-//   let h = canvasHeight / 2;
-//   drawEllipse(projector.ofApp, x, y, w, h, [0, 0, 255, 1], true);
-// }, 5000);
+  // var runInt = setInterval(function() {
+  //   console.log('alive');
+  // }, 5000);
+
+  var runInt = setInterval(function() {
+    socket.emit('command', null);
+  }, 5000);
+};
+
+runLoop();
