1f4730c9a636a2223883fc6e124562dd3eca2478
Mateo Garcia <mateog@stanford.edu>
2018-07-10 18:19:03 -0700

    drawLine command functional. Testing drawShape

--- app.js
+++ app.js
@@ -59,20 +59,26 @@ var runLoop = function() {
   // }, 1000);
 
   // Draw two sides of an obtuse blue triangle from top left to center to middle right
+  // every 5 s
+  // var runInt = setInterval(function() {
+  //   socket.emit('command', {
+  //     command: 'drawLine',
+  //     vertices: [[0, 0], [canvasWidth / 2, canvasHeight / 2], [canvasWidth, canvasHeight / 2]],
+  //     color: [0, 0, 255, 1]
+  //   })
+  // }, 5000);
+
+  // Draw a filled blue triangle from top left to center to middle left every 5 s
   var runInt = setInterval(function() {
     socket.emit('command', {
-      command: 'drawLine',
-      vertices: [[0, 0], [canvasWidth / 2, canvasHeight / 2], [canvasWidth, canvasHeight / 2]],
-      color: [0, 0, 255, 1]
+      command: 'drawShape',
+      vertices: [[0, 0], [canvasWidth / 2, canvasHeight / 2], [0, canvasHeight / 2]],
+      color: [0, 0, 255, 1],
+      shouldFill: true
     })
   }, 5000);
 
-  // Draw a filled blue triangle from top left to center to middle left
-  // var runInt = setInterval(function() {
-  //   drawShape(projector.ofApp, [[0, 0], [canvasWidth / 2, canvasHeight / 2], [0, canvasHeight / 2]], [0, 0, 255, 1], true);
-  // }, 5000);
-
-  // Draw a filled blue ellipse in the center of the screen.
+  // Draw a filled blue ellipse in the center of the screen every 5 s
   // var runInt = setInterval(function() {
   //   let x = canvasWidth / 2;
   //   let y = canvasHeight / 2;
