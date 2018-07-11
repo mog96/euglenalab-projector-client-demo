6beab6f3a21d53b474750da3b8bd0a876847b03e
Mateo Garcia <mateog@stanford.edu>
2018-07-10 18:22:00 -0700

    drawShape command functional. Testing drawEllipse

--- app.js
+++ app.js
@@ -69,23 +69,27 @@ var runLoop = function() {
   // }, 5000);
 
   // Draw a filled blue triangle from top left to center to middle left every 5 s
+  // var runInt = setInterval(function() {
+  //   socket.emit('command', {
+  //     command: 'drawShape',
+  //     vertices: [[0, 0], [canvasWidth / 2, canvasHeight / 2], [0, canvasHeight / 2]],
+  //     color: [0, 0, 255, 1],
+  //     shouldFill: true
+  //   })
+  // }, 5000);
+
+  // Draw a filled blue ellipse in the center of the screen every 5 s
   var runInt = setInterval(function() {
     socket.emit('command', {
-      command: 'drawShape',
-      vertices: [[0, 0], [canvasWidth / 2, canvasHeight / 2], [0, canvasHeight / 2]],
+      command: 'drawEllipse',
+      x: canvasWidth / 2,
+      y: canvasHeight / 2,
+      w: canvasWidth / 2,
+      h: canvasHeight / 2,
       color: [0, 0, 255, 1],
       shouldFill: true
     })
   }, 5000);
-
-  // Draw a filled blue ellipse in the center of the screen every 5 s
-  // var runInt = setInterval(function() {
-  //   let x = canvasWidth / 2;
-  //   let y = canvasHeight / 2;
-  //   let w = canvasWidth / 2;
-  //   let h = canvasHeight / 2;
-  //   drawEllipse(projector.ofApp, x, y, w, h, [0, 0, 255, 1], true);
-  // }, 5000);
 };
 
 runLoop();
